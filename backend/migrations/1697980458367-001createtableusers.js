3512365097830f8d02f185a2b08845eb67d9a5ce
Timofey Kuznetsov <kuznetsov.timofey.2003@mail.ru>
2023-10-23 00:28:16 +1000

    fix user table

--- backend/migrations/1697980458367-001createtableusers.js
+++ backend/migrations/1697980458367-001createtableusers.js
@@ -2,11 +2,13 @@ exports.up = (pgm) => {
   pgm.createTable('users', {
     id: 'id',
     name: { type: 'varchar(1000)', notNull: true },
-    createdAt: {
+    email: {type: 'varchar(1000)', notNull: true },
+    age: 'integer',
+    dob: {
       type: 'timestamp',
       notNull: true,
-      default: pgm.func('current_timestamp')
-    }
+      default: pgm.func('current_timestamp'),
+    },
   });
 };
 
--- backend/src/user/queries.js
+++ backend/src/user/queries.js
@@ -1,9 +1,9 @@
-const getAllUsers = "SELECT * FROM students";
-const getUsersById = "SELECT * FROM students WHERE id = $1";
-const checkEmailExist = "SELECT s FROM students s WHERE s.email = $1";
-const addUser = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)"; 
-const deleteUser = "DELETE FROM students WHERE id = $1";
-const updateUser = "UPDATE students SET name = $1 WHERE id = $2"
+const getAllUsers = "SELECT * FROM users";
+const getUsersById = "SELECT * FROM users WHERE id = $1";
+const checkEmailExist = "SELECT s FROM users s WHERE s.email = $1";
+const addUser = "INSERT INTO users (name, email, age, dob) VALUES ($1, $2, $3, $4)"; 
+const deleteUser = "DELETE FROM users WHERE id = $1";
+const updateUser = "UPDATE users SET name = $1 WHERE id = $2"
 
 module.exports = {
     getAllUsers,
